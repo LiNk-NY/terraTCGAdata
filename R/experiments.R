f9f59ef19a6557401cc473a615eefe37c29c6839
LiNK-NY <marcel.ramosperez@roswellpark.org>
2021-12-30 21:46:26 +0000

    add conditional code for seg data

--- R/experiments.R
+++ R/experiments.R
@@ -27,7 +27,7 @@ getAssayTable <-
 #'     e.g., "01" for Primary Solid Tumors, see
 #'     `data("sampleTypes", package = "TCGAutils")` for reference
 #'
-#' @return An `ExperimentList` of assays selected
+#' @return Either a matrix or RaggedExperiment depending on the assay selected
 #'
 #' @examples
 #'
@@ -71,35 +71,94 @@ getAssayData <-
     }
 
     assaycopied <- list.files(rpath, pattern = "\\.txt", full.names = TRUE)
-    tcgaids <- gsub(".data.txt", "", basename(assaycopied), fixed = TRUE)
-    tcgaids <- tcgaids[
-        TCGAutils::TCGAsampleSelect(tcgaids, sampleCodes = sampleCode)
-    ]
-    assayselect <- file.path(rpath, paste0(tcgaids, ".data.txt"))
+    suffix <-
+        if (grepl("^cna|snp", assayName)) { ".seg.txt" } else { ".data.txt" }
+    tcgaids <- gsub(suffix, "", basename(assaycopied), fixed = TRUE)
+    if (!missing(sampleCode) && .is_character(sampleCode))
+        tcgaids <- tcgaids[
+            TCGAutils::TCGAsampleSelect(tcgaids, sampleCodes = sampleCode)
+        ]
+    assayselect <- file.path(rpath, paste0(tcgaids, suffix))
     datarows <- lapply(
         assayselect,
         readr::read_tsv,
         show_col_types = FALSE
     )
-    cnames <- unlist(datarows[[1]][1, ], use.names = FALSE)
-    datarows <- lapply(datarows, function(x) x[-1, ])
-    rowlist <- lapply(datarows, `[[`, 1L)
-    dataonly <- lapply(datarows, function(x) {
-        suppressMessages(readr::type_convert(x[2L]))
-    })
-    dups <- rowlist[!duplicated(rowlist)]
-    logilist <- vector("list", length(dups))
-    bindlist <- vector("list", length(dups))
-    for (i in seq_along(dups)) {
-        logilist[[i]] <-
-            vapply(rowlist, function(rl) identical(rl, dups[[i]]), logical(1L))
-        bindlist[[i]] <-
-            cbind(rownames = dups[[i]],
-                dplyr::bind_cols(dataonly[logilist[[i]]]))
+    if (grepl("seg", suffix)) {
+        .mergeRangedSamples(datarows)
+    } else {
+        cnames <- unlist(datarows[[1]][1, ], use.names = FALSE)
+        datarows <- lapply(datarows, function(x) x[-1, ])
+        rowlist <- lapply(datarows, `[[`, 1L)
+        dataonly <- lapply(datarows, function(x) {
+            suppressMessages(readr::type_convert(x[2L]))
+        })
+        dups <- rowlist[!duplicated(rowlist)]
+        logilist <- vector("list", length(dups))
+        bindlist <- vector("list", length(dups))
+        for (i in seq_along(dups)) {
+            logilist[[i]] <-
+                vapply(
+                    rowlist,
+                    function(rl) identical(rl, dups[[i]]),
+                    logical(1L)
+                )
+            bindlist[[i]] <-
+                cbind(rownames = dups[[i]],
+                    dplyr::bind_cols(dataonly[logilist[[i]]]))
+        }
+        df <- Reduce(function(...) {
+            merge(..., all = TRUE, by = "rownames")
+        }, bindlist)
+        rownames(df) <- df[["rownames"]]
+        data.matrix(df[, -1])
     }
-    df <- Reduce(function(...) {
-        merge(..., all = TRUE, by = "rownames")
-    }, bindlist)
-    rownames(df) <- df[["rownames"]]
-    data.matrix(df[, -1])
+}
+
+#' Import Terra TCGA data as an ExperimentList
+#'
+#' @inheritParams getAssayData
+#'
+#' @param assayNames character() A vector of assays selected from the colnames
+#'     of `getAssayTable`.
+#'
+#' @return An `ExperimentList` of assays
+#'
+#' @examples
+#'
+#' ExperimentListData(
+#'     assayNames = c("protein_exp__mda_rppa_core__mdanderson_org__Level_3__protein_normalization__data",
+#'     "snp__genome_wide_snp_6__broad_mit_edu__Level_3__segmented_scna_minus_germline_cnv_hg18__seg"),
+#'     sampleCode = c("01", "10")
+#' )
+#'
+#' @export
+ExperimentListData <- function(assayNames, sampleCode) {
+    exps <- lapply(
+        stats::setNames(nm = assayNames), getAssayData, sampleCode = sampleCode
+    )
+    ExperimentList(exps)
+}
+
+#' @examples
+#'
+#' terraTCGAdata(
+#'     assays = c("protein_exp__mda_rppa_core__mdanderson_org__Level_3__protein_normalization__data",
+#'     "rnaseqv2__illuminahiseq_rnaseqv2__unc_edu__Level_3__RSEM_genes_normalized__data"),
+#'     sampleCode = c("01", "10")
+#' )
+#'
+#' @export
+terraTCGAdata <- function(assays, sampleCode = NULL, split = TRUE) {
+    el <- ExperimentListData(assayNames = assays, sampleCode = sampleCode)
+    coldata <- getClinical()
+    samap <- TCGAutils::generateMap(el, coldata, TCGAutils::TCGAbarcode)
+    mae <- MultiAssayExperiment(
+        experiments = el, colData = coldata, sampleMap = samap
+    )
+    if (split && (length(sampleCode) || !is.null(sampleCode))) {
+        TCGAsplitAssays(mae, sampleCodes = sampleCode, exclusive = TRUE)
+    } else {
+        mae
+    }
 }
