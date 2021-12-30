dbc673b8b275bfb5a8ccd51164d27ad3ee1450d6
LiNK-NY <marcel.ramosperez@roswellpark.org>
2021-12-30 21:46:51 +0000

    add helper to handle ranged data

--- R/utilities.R
+++ R/utilities.R
@@ -12,3 +12,17 @@
 .is_scalar_logical <- function(x, na.ok = FALSE) {
     is.logical(x) && length(x) == 1L && (na.ok || !is.na(x))
 }
+
+.mergeRangedSamples <- function(sample_list) {
+    sample_names <- vapply(
+        sample_list, function(x) { unique(x[["Sample"]]) }, character(1L)
+    )
+    samples <- lapply(sample_list, function(x) x[, names(x) != "Sample"])
+    names(samples) <- sample_names
+    listgranges <- lapply(
+        samples, GenomicRanges::makeGRangesFromDataFrame,
+        keep.extra.columns = TRUE
+    )
+    grls <- as(listgranges, "GRangesList")
+    RaggedExperiment::RaggedExperiment(grls)
+}
