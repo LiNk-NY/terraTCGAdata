a1aa706fcaac67b099b48badc7e224aa835bf66a
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-02-08 00:26:39 +0000

    wip: update workspaces

--- R/terraTCGAworkspaces.R
+++ R/terraTCGAworkspaces.R
@@ -6,5 +6,25 @@ findTCGAworkspaces <- function(project = "TCGA", cancerCode = ".*") {
 
 setTCGAworkspace <- function(projectCode = NULL) {
     ## TODO: set package-wide option and/or ENV for namespace / name
-    getOption("terraTCGAdata.workspace", projectCode)
+    getOption("terraTCGAdata.workspace", terraSetWorkspace())
+}
+
+terraSetWorkspace <- function(projectCode) {
+    ws <- getOption("terraTCGAdata.workspace")
+    if (!nzchar(ws)) {
+        tcga_choices <- findTCGAworkspaces()
+        if (!missing(projectCode)) {
+            validPC <- projectCode %in% tcga_choices
+            if (!validPC)
+                stop("'projectCode' not in the 'findTCGAworkspaces()' list ")
+        } else {
+            ws <- menu(
+                tcga_choices,
+                title = "Select a TCGA terra Workspace: "
+            )
+            ws <- tcga_choices[ws]
+        }
+        options("terraTCGAdata.workspace" = ws)
+    }
+    ws
 }
