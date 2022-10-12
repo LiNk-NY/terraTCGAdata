20da12c4a9e6e22101d1ce33e6da9e8c25e3a116
LiNk-NY <marcel.ramosperez@roswellpark.org>
2022-10-12 16:54:50 -0400

    update unit tests

--- tests/testthat/test-terraTCGAworkspaces.R
+++ tests/testthat/test-terraTCGAworkspaces.R
@@ -1,36 +1,39 @@
 test_that("terraTCGAworkspace works", {
+    skip_if_not(AnVIL::gcloud_exists())
+    ## Blank not in validation table
     withr::with_options(
         list("terraTCGAdata.workspace" = ""),
+        expect_error(
+            terraTCGAworkspace()
+        )
+    )
+
+    withr::with_options(
+        list("terraTCGAdata.workspace" = NULL),
         expect_warning(
             terraTCGAworkspace()
         )
     )
-    
+
     withr::with_options(
-        list("terraTCGAdata.workspace" = NULL), 
-        expect_warning(
-            terraTCGAworkspace()
-        )
-    )
-    
-    withr::with_options(
-        list("terraTCGAdata.workspace" = "TCGA_ACC_OpenAccess_V1-0_DATA"), 
+        list("terraTCGAdata.workspace" = "TCGA_ACC_OpenAccess_V1-0_DATA"),
         expect_identical(
             terraTCGAworkspace(),
             "TCGA_ACC_OpenAccess_V1-0_DATA"
         )
     )
-    
-    ## No OP when option is already set
-    withr::with_options(
-        list("terraTCGAdata.workspace" = "TCGA_ACC_OpenAccess_V1-0_DATA"), 
-        expect_identical(
-            terraTCGAworkspace("TCGA_TGCT_OpenAccess_V1-0_DATA"),
-            "TCGA_ACC_OpenAccess_V1-0_DATA"
+
+    ## Replacement when input provided
+    expect_warning(
+        withr::with_options(
+            list("terraTCGAdata.workspace" = "TCGA_ACC_OpenAccess_V1-0_DATA"),
+            expect_identical(
+                terraTCGAworkspace("TCGA_TGCT_OpenAccess_V1-0_DATA"),
+                "TCGA_TGCT_OpenAccess_V1-0_DATA"
+            )
         )
     )
-    
-    skip_if_not(AnVIL::gcloud_exists()) 
+
     expect_error(
         terraTCGAworkspace("ABDC")
     )
